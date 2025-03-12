9a8e2b92cf2bf0b27bda34b4c3dfba22103f2b89
Egor Chichin <chichin@inbox.ru>
2025-03-12 16:55:19 +0300

    1 commit

---
+++ src/main/java/six_chapter/Point.java
@@ -0,0 +1,17 @@
+package six_chapter;
+
+public class Point {
+    //поля класса
+    public int x; // абсцисса точки
+    public int y; // ордината точки
+
+    // конструктор, создающий точку с указанными координатами
+    Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+    // возвращает строку с описанием точки
+    public String toString() {
+        return String.format("{%s;%s}", x, y);
+    }
+}
---
+++ src/main/java/six_chapter/Start.java
@@ -0,0 +1,26 @@
+package six_chapter;
+
+public class Start {
+    public static void main(String[] args) {
+        //Задание 1 - точка координат, сравнение точек
+        Point p1 = new Point(1,3);//точка 1
+        Point p2 = new Point(1,3);//точка 2
+        Point p3 = new Point(5,8);//точка 3
+
+        System.out.print(p1 + " ");
+        System.out.print(((p1.x) == (p3.x)) && ((p1.y) == (p3.y)));//сравнение первой и третьей
+        System.out.println(" " + p3);
+
+        System.out.print(p2 + " ");
+        System.out.print(((p2.x) == (p3.x)) && ((p2.y) == (p3.y)));//сравнение второй и третьей
+        System.out.println(" " + p3);
+
+        System.out.print(p1 + " ");
+        System.out.print(((p1.x) == (p2.x)) && ((p1.y) == (p2.y)));//сравнение первой и второй
+        System.out.println(" " + p2);
+
+
+        Line line = new Line(1, 2, 3, 4);
+        System.out.println(line.toString());
+    }
+}
