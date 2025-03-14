1185c2dd6ef1bc6ec0adb2c0f3cf7bdc309a3b74
Egor Chichin <chichin@inbox.ru>
2025-03-14 16:17:03 +0300

    lesson Line - внесены текстовые правки - разделители

--- src/main/java/six_chapter/Start.java
+++ src/main/java/six_chapter/Start.java
@@ -9,6 +9,7 @@ public class Start {
         Point p4 = new Point(10,11);//точка 2
         Point p5 = new Point(15,19);//точка 3
 
+        System.out.println("Точки: "); //Задание на вывод и равнение точек
         System.out.print(p1 + " ");
         System.out.print(((p1.x) == (p3.x)) && ((p1.y) == (p3.y)));//сравнение первой и третьей
         System.out.println(" " + p3 + " - сравнение 1 и 3 точек");
@@ -21,15 +22,23 @@ public class Start {
         System.out.print(((p1.x) == (p2.x)) && ((p1.y) == (p2.y)));//сравнение первой и второй
         System.out.println(" " + p2 + " - сравнение 1 и 2 точек");
 
+        System.out.println(" "); //Переход от задания точки к заданию линии
+        System.out.println("Линии: "); //Переход от задания точки к заданию линии
+
         Line line1 = new Line(p1, p3);
-        //System.out.println(line1.toString());
+        System.out.println(line1.toString());
         Line line2 = new Line(p4, p5);
-        //System.out.println(line2.toString());
+        System.out.println(line2.toString());
         Line line3 = new Line(p3, p4);
         System.out.println(line3.toString() + " - Начальные координаты линии 3");
 
+        System.out.println("Изменение координат начала и конца линии 3");
         p3 = new Point(9,12);
         p4 = new Point(13,16);
+        line1 = new Line(p1, p3);
+        System.out.println(line1.toString());
+        line2 = new Line(p4, p5);
+        System.out.println(line2.toString());
         line3 = new Line(p3, p4);
         System.out.println(line3.toString() + " - Новые координаты линии 3");
         double dis1=Math.sqrt((p1.x-p3.x)*(p1.x-p3.x) + (p1.y-p3.y)*(p1.y-p3.y));
