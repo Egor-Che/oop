d3f4702b766c32af006d7e6bd32e97fa75aec1ef
Egor Chichin <chichin@inbox.ru>
2025-04-01 16:37:51 +0300

    Практика ООП. Полиморфизм 1. Точка 3d - простейшее решение

---
+++ src/main/java/eight_chapter/Start.java
@@ -0,0 +1,8 @@
+package eight_chapter;
+
+public class Start {
+    public static void main(String[] args) {
+        Point3D point = new Point3D(2, 4, 6);
+        System.out.println("X: " + point.x + ", Y: " + point.y + ", Z: " + point.z);
+    }
+}
