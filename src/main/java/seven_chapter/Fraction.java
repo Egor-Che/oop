ecef2023e151d1deaf3c90cda07b55e93652c7dc
Egor Chichin <chichin@inbox.ru>
2025-04-06 14:26:53 +0300

    Практика ООП. Полиморфизм 5. Изменен класс Fraction с наследованием от класса Number

--- src/main/java/seven_chapter/Fraction.java
+++ src/main/java/seven_chapter/Fraction.java
@@ -1,19 +1,22 @@
 package seven_chapter;
-public class Fraction {
+public class Fraction extends Number {
     private final int numerator;
     private final int denominator;
     private final float fraction;
+
     public Fraction(int numerator, int denominator) {
-        if (denominator <= 0){
+        if (denominator <= 0) {
             throw new IllegalArgumentException("Знаменатель не может быть отрицательным, или равным нулю");
         }
         this.numerator = numerator;
         this.denominator = denominator;
         this.fraction = (float) numerator / (float) denominator;
     }
+
     public int getNumerator() {
         return numerator;
     }
+
     public int getDenominator() {
         return denominator;
     }
@@ -30,11 +33,12 @@ public class Fraction {
         }
         return new Fraction(numTmp, denTmp);
     }
+
     public Fraction minus(Fraction fraction) {
         int numTmp;
         int denTmp;
         if (this.getDenominator() == fraction.getDenominator()) {
-            numTmp = this.getNumerator() + fraction.getNumerator();
+            numTmp = this.getNumerator() - fraction.getNumerator();
             denTmp = this.getDenominator();
         } else {
             numTmp = this.getNumerator() * fraction.getDenominator() - fraction.getNumerator() * this.getDenominator();
@@ -51,15 +55,46 @@ public class Fraction {
                 ", fraction=" + fraction +
                 '}';
     }
-    // The msg is optional here
+
     public void print(String msg) {
-        // Print space for each character in the string
         for (int i = 0; i <= msg.length(); ++i)
             System.out.print(" ");
-        System.out.println(" "+ numerator);
+        System.out.println(" " + numerator);
         System.out.println(msg + " --- = " + fraction);
         for (int i = 0; i <= msg.length(); ++i)
             System.out.print(" ");
-        System.out.println(" "+ denominator);
+        System.out.println(" " + denominator);
+    }
+
+    // Реализация абстрактных методов Number
+    @Override
+    public int intValue() {
+        return (int) fraction;
+    }
+
+    @Override
+    public long longValue() {
+        return (long) fraction;
+    }
+
+    @Override
+    public float floatValue() {
+        return fraction;
+    }
+
+    @Override
+    public double doubleValue() {
+        return (double) fraction;
+    }
+
+    // Можно переопределить byteValue и shortValue для более точного представления
+    @Override
+    public byte byteValue() {
+        return (byte) fraction;
+    }
+
+    @Override
+    public short shortValue() {
+        return (short) fraction;
     }
 }
